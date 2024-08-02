6878a3147630903e3cde2ac8e2779cc892e2d3ea
Daanturo <daanturo@gmail.com>
2024-08-03 01:06:19 +0700

    Copy to clipboard

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use ashpd::desktop::Color;
+use cli_clipboard;
 use futures::executor;
 
 fn pick_color() -> (f64, f64, f64) {
@@ -13,11 +14,17 @@ fn pick_color() -> (f64, f64, f64) {
 }
 
 fn main() {
-    let color = pick_color();
+    // 256
     let max_val = 0x100 as f64;
+
+    let color = pick_color();
     let R = (color.0 * max_val).round() as i16;
     let G = (color.1 * max_val).round() as i16;
     let B = (color.2 * max_val).round() as i16;
     let color_code = format!("#{:02X}{:02X}{:02X}", R, G, B);
+
     println!("{}", color_code);
+
+    cli_clipboard::set_contents(color_code.to_owned()).unwrap();
+    assert_eq!(cli_clipboard::get_contents().unwrap(), color_code);
 }
